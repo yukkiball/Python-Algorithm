f87a19c26d1bebdb4cb4f483506537680571dc6e
yukkiball <dairuilin@126.com>
2019-05-21 19:55:14 +0800

    Sort

---
+++ 排序/快速排序.py
@@ -0,0 +1,29 @@
+import random
+def quick_sort(listunsort, lo, hi):
+    """快速排序"""
+    if lo >= hi:
+        return
+    j = partition(listunsort, lo, hi)
+    quick_sort(listunsort, lo, j - 1)
+    quick_sort(listunsort, j + 1, hi)
+
+def partition(listunsort, lo, hi):
+    """寻找轴点"""
+    mid = listunsort[lo]
+    while lo < hi:
+        while lo < hi and listunsort[hi] >= mid:
+            hi -= 1
+        listunsort[lo] = listunsort[hi]
+
+        while lo < hi and listunsort[lo] <= mid:
+            lo += 1
+        listunsort[lo] = listunsort[hi]
+
+    listunsort[lo] = mid
+    return lo
+
+#测试
+l1 = [5, 3, 1, 2, 6, 8, 6]
+random.shuffle(l1)
+quick_sort(l1, 0, len(l1) - 1)
+print(l1)
